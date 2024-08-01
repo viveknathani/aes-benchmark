3727f9018515b0af8630c5357dd42e2910df0f18
viveknathani <viveknathani2402@gmail.com>
2024-08-01 15:17:34 +0530

    chore: basic working AES

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # aes-benchmark
 
-An attempt to benchmark various AES implementations in Rust.
+An attempt to benchmark various AES implementations in Rust to encrypt 150MB of random data.
+
+### libraries being tested
+
+1. [rust-crypto](https://github.com/DaGenix/rust-crypto)
+
+### modes being tested
+
+1. [CTR](https://en.wikipedia.org/wiki/Block_cipher_mode_of_operation#Counter_(CTR))
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::time::Instant;
+use rand::RngCore;
+
+// For rust-crypto
+use crypto::aes::{ctr, KeySize};
+use crypto::symmetriccipher::SynchronousStreamCipher;
+
+const DATA_SIZE: usize = 150 * 1024 * 1024; // 150MB
+
 fn main() {
-    println!("Hello, world!");
+    let mut data = vec![0u8; DATA_SIZE];
+    rand::thread_rng().fill_bytes(&mut data);
+
+    let key = [0u8; 32]; // 256-bit key
+    let nonce = [0u8; 16]; // AES block size
+
+    println!("Benchmarking AES implementations for 150MB of data:");
+
+    // Rust-crypto CTR
+    let start = Instant::now();
+    let mut rust_crypto_ctr = ctr(KeySize::KeySize256, &key, &nonce);
+    let mut output = vec![0u8; DATA_SIZE];
+    rust_crypto_ctr.process(&data, &mut output);
+    println!("Rust-crypto CTR: {:?}", start.elapsed());
 }
